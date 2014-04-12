ffc0e51e15eead7ad3e1ad84bff04c2c8d6fc128
Megan Byrne <megan.byrne27@gmail.com>
2014-04-12 15:13:52 -0700

    Add basic CRUD actions for Lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      flash[:notice] = "To Do List saved."
+      redirect_to @list
+    else
+      flash[:error] = "There was an error saving your To Do List. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+
+    if @list.update(list_params)
+      redirect_to @list
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:title)
+    end
+end
