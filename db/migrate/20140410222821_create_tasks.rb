6ca81ab7638ff9a5025b495fc8fbe1daa0e42fcf
Megan Byrne <megan.byrne27@gmail.com>
2014-04-21 08:46:38 -0700

    reset migrations

--- db/migrate/20140410222821_create_tasks.rb
+++ db/migrate/20140410222821_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :description
-      t.integer :time_goal
+      t.datetime :time_goal
       t.boolean :completed, default: false 
       t.references :list, index: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418153547) do
+ActiveRecord::Schema.define(version: 20140418152444) do
 
   create_table "lists", force: true do |t|
     t.string   "title"
