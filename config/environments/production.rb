236e982cdd98c7facfbb66629328d8ba40eacf47
Megan Byrne <megan.byrne27@gmail.com>
2014-04-25 08:46:41 -0700

    Change production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile += %w( .svg )
+  #config.assets.precompile += %w( .svg )
+  config.assets.precompile << /\.(?:svg)$/
   # Generate digests for assets URLs.
   config.assets.digest = true
 
