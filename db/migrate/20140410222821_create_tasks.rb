b090ca55f663083a641e3c4deb9d84d41639fe1b
Megan Byrne <megan.byrne27@gmail.com>
2014-04-30 08:01:09 -0700

    change task to date from datetime

--- db/migrate/20140410222821_create_tasks.rb
+++ db/migrate/20140410222821_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :description
-      t.datetime :time_goal
+      t.date :time_goal
       t.boolean :completed, default: false 
       t.references :list, index: true
 
